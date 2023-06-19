e6468932f04520fd072697c84ec5e6a495e589f5
Raizel-2023 <111248812+Raizel-2023@users.noreply.github.com>
2023-06-19 12:00:52 +0300

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,14 +1,14 @@
 const { Insta,cmd } = require('../lib')
 cmd({
-        pattern: "insta",
+        pattern: "انستا",
         desc: "download instagram post.",
         category: "downloader",
         filename: __filename
     },
     async(Void, citel,text,{isCreator}) => {
-if(!text) return citel.reply('Need post url.')
+if(!text) return citel.reply('ارسل الامر مع الرابط.')
 let response = await Insta(text)
 for (let i=0;i<response.length;i++) {
-await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
+await Void.sendFileUrl(citel.chat, response[i], `*جار التحميل.*`, citel)
 }
     });
